26938511efc46e9f08d9d82f8ac1a362a18c688d
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-06-17 13:58:02 +0530

    add setup script for qemu-kvm

---
+++ qemu-kvm-manage/setup-qemu-kvm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "\nEnabling passwordless sudo for $USER . . .\n"
+cat <<EOF | sudo tee "/etc/sudoers.d/$USER"
+$USER ALL=(ALL) NOPASSWD: ALL
+Defaults:$USER !authenticate
+EOF
+
+echo -e "\nInstall required packages for qemu-kvm . . . \n"
+
+sudo dnf install -y qemu-kvm qemu-img libvirt libvirt-daemon libvirt-daemon-driver-qemu bridge-utils python3-libxml2 python3-libvirt libosinfo python3-gobject gobject-introspection edk2-ovmf 
+
+echo -e "\nEnable and start libvirtd . . . \n"
+systemctl enable --now libvirtd
+systemctl status libvirtd
+sudo usermod -aG libvirt $USER
+sudo newgrp libvirt
+
+echo -e "\nCreating /virtual-machines to manage VMs . . . \n"
+sudo mkdir -p /virtual-machines
+sudo chown -R $USER:qemu /virtual-machines
+chmod -R g+s /virtual-machines
+
+echo -e "Clone https://github.com/virt-manager/virt-manager.git repo to /virtual-machines/virt-manager .  . . \n"
+mkdir -p /virtual-machines/virt-manager && \
+git clone https://github.com/virt-manager/virt-manager.git /virtual-machines/virt-manager
+
+echo -e "\nCreate a wrapper binary for /bin/virt-install from /virtual-machines/virt-manager . . .\n"
+cat <<EOF | sudo tee /bin/virt-install
+#!/bin/bash
+PYTHONPATH=/virtual-machines/virt-manager exec python3 /virtual-machines/virt-manager/virt-install "\$@"
+EOF
+sudo chmod +x /bin/virt-install
+
+echo -e "\nSetup Custom Bridge Network virbr0 defined in virbr0.xml . . .\n" 
+sudo virsh net-destroy default &>/dev/null
+sudo virsh net-undefine default &>/dev/null
+sudo virsh net-define virbr0.xml
+sudo virsh net-start default
+sudo virsh net-autostart default
+sudo virsh net-dumpxml default
