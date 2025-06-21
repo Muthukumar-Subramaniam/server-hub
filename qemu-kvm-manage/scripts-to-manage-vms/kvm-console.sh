2c7586f5453a4ba9b34cedea79d9cefab429f4ca
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-06-21 06:55:06 +0530

    Add additional scripts-to-manage-vms

---
+++ qemu-kvm-manage/scripts-to-manage-vms/kvm-console.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ "$EUID" -eq 0 ]]; then
+    echo -e "\n⛔ Running as root user is not allowed."
+    echo -e "\n🔐 This script should be run as a user who has sudo privileges, but *not* using sudo.\n"
+    exit 1
+fi
+
+# Check if we're inside a QEMU guest
+if sudo dmidecode -s system-manufacturer | grep -qi 'QEMU'; then
+    echo "❌❌❌  FATAL: WRONG PLACE, BUDDY! ❌❌❌"
+    echo -e "\n⚠️  Note:"
+    echo -e "  🔹 This script is meant to be run on the *host* system managing QEMU/KVM VMs."
+    echo -e "  🔹 You’re currently inside a QEMU guest VM, which makes absolutely no sense.\n"
+    echo "💥 ABORTING EXECUTION 💥"
+    exit 1
+fi
+
+# Use first argument or prompt for hostname
+if [ -n "$1" ]; then
+    qemu_kvm_hostname="$1"
+else
+    read -rp "⌨️ Please enter the Hostname of the VM to take console : " qemu_kvm_hostname
+fi
+
+# Check if VM exists in 'virsh list --all'
+if ! sudo virsh list --all | awk '{print $2}' | grep -Fxq "$qemu_kvm_hostname"; then
+    echo "❌ Error: VM \"$qemu_kvm_hostname\" does not exist."
+    exit 1
+fi
+
+# Check if VM exists in 'virsh list'
+if ! sudo virsh list  | awk '{print $2}' | grep -Fxq "$qemu_kvm_hostname"; then
+    echo "❌ Error: VM \"$qemu_kvm_hostname\" is not running."
+    exit 1
+fi
+
+# Proceed to take console
+sudo virsh console "${qemu_kvm_hostname}" 2>/dev/null
