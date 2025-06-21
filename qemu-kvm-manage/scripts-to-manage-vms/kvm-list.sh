33a1f7290ae57a244307842bbbd8a12cc75340be
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-06-21 07:08:09 +0530

    add scripts-to-manage-vms/kvm-list.sh

---
+++ qemu-kvm-manage/scripts-to-manage-vms/kvm-list.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$EUID" -eq 0 ]]; then
+    echo -e "\n⛔ Running as root user is not allowed."
+    echo -e "\n🔐 This script should be run as a user who has sudo privileges, but *not* using sudo.\n"
+    exit 1
+fi
+
+# Check if we're inside a QEMU guest
+if sudo dmidecode -s system-manufacturer | grep -qi 'QEMU'; then
+    echo "❌❌❌  FATAL: WRONG PLACE, BUDDY! ❌❌❌"
+    echo -e "\n⚠️  Note:"
+    echo -e "  🔹 This script is meant to be run on the *host* system managing QEMU/KVM VMs."
+    echo -e "  🔹 You’re currently inside a QEMU guest VM, which makes absolutely no sense.\n"
+    echo "💥 ABORTING EXECUTION 💥"
+    exit 1
+fi
+
+sudo virsh list --all
