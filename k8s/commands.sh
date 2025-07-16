7aea6fbb75d0780d20a00d25b6c228277356a842
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-07-16 15:06:43 +0530

    update local domain name used for testing in k8s related manifests and scripts

--- k8s/commands.sh
+++ k8s/commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #To List pods on a specific node
-kubectl get pods --all-namespaces --field-selector spec.nodeName=k8s-cp1.ms.local
-kubectl get pods --all-namespaces --field-selector spec.nodeName=k8s-w1.ms.local
-kubectl get pods --all-namespaces --field-selector spec.nodeName=k8s-w2.ms.local
+kubectl get pods --all-namespaces --field-selector spec.nodeName=k8s-cp1.lab.local
+kubectl get pods --all-namespaces --field-selector spec.nodeName=k8s-w1.lab.local
+kubectl get pods --all-namespaces --field-selector spec.nodeName=k8s-w2.lab.local
 
