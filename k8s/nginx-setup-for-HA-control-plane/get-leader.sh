a987f5004099d09fc1080aa90f979898d3976ef8
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-08-18 19:03:31 +0530

    add some k8s related scripts and configs

---
+++ k8s/nginx-setup-for-HA-control-plane/get-leader.sh
@@ -0,0 +1,6 @@
+for i in $(cat control-plane-nodes); do echo "Host : $i";ssh "$i" "kubectl exec -n kube-system etcd-$i -- etcdctl \
+--endpoints=https://127.0.0.1:2379 \
+--cacert=/etc/kubernetes/pki/etcd/ca.crt \
+--cert=/etc/kubernetes/pki/etcd/server.crt \
+--key=/etc/kubernetes/pki/etcd/server.key \
+endpoint status --write-out=table";echo;done
