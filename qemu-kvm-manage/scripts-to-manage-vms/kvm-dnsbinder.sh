28ca5d30bb739986d1f47badc42c5f220cae25e7
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-09-23 11:02:05 +0000

    bug fix : kvm-dnsbinder

--- qemu-kvm-manage/scripts-to-manage-vms/kvm-dnsbinder.sh
+++ qemu-kvm-manage/scripts-to-manage-vms/kvm-dnsbinder.sh
@@ -25,7 +25,7 @@ infra_mgmt_super_username=$(< /kvm-hub/infra-mgmt-super-username)
 local_infra_domain_name=$(< /kvm-hub/local_infra_domain_name)
 
 
-echo -n "\n⚙️  Enabling DNS of lab infra with resolvectl if required . . . "
+echo -n -e "\n⚙️  Enabling DNS of lab infra with resolvectl if required . . . "
 
 if grep -q "${infra_server_ipv4_address}" <<< $(resolvectl); then
     echo "[ ✅ ]"
