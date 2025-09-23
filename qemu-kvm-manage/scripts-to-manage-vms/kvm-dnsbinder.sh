6d00f4f79b507ca924cd8167b0eaadae885928a5
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-09-23 11:15:05 +0000

    bug fix : kvm-dnsbinder

--- qemu-kvm-manage/scripts-to-manage-vms/kvm-dnsbinder.sh
+++ qemu-kvm-manage/scripts-to-manage-vms/kvm-dnsbinder.sh
@@ -28,18 +28,18 @@ local_infra_domain_name=$(< /kvm-hub/local_infra_domain_name)
 echo -n -e "\n⚙️  Enabling DNS of lab infra with resolvectl if required . . . "
 
 if grep -q "${infra_server_ipv4_address}" <<< $(resolvectl); then
-    echo "[ ✅ ]"
+    echo -e "\e[32m[ ok ]\e[0m"
 else
     if ip link show virbr0 &>/dev/null; then
        sudo resolvectl dns virbr0 ${infra_server_ipv4_address}
        sudo resolvectl domain virbr0 ${local_infra_domain_name} 
-       echo "[ ✅ ]"
+       echo -e "\e[32m[ done ]\e[0m"
     else
        echo -e "\n❌ virbr0 interface is not yet available! \n" 
        exit 1
     fi
 fi
 
-echo -e "\n⚙️  Invoking dnsbinder utility from lab infra server . . .\n"
+echo -e "\n⚙️  Invoking dnsbinder utility from lab infra server . . ."
 
 ssh -o LogLevel=QUIET -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -t ${infra_mgmt_super_username}@${infra_server_ipv4_address} "sudo dnsbinder $@"
