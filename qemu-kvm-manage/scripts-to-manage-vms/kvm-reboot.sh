1073246014a288ca4a86e3e1668bf888039fd60f
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-11-30 03:40:10 +0000

    add new tools kvm-shutdown and kvm-reboot

---
+++ qemu-kvm-manage/scripts-to-manage-vms/kvm-reboot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#----------------------------------------------------------------------------------------#
+# If you encounter any issues with this script, or have suggestions or feature requests, #
+# please open an issue at: https://github.com/Muthukumar-Subramaniam/server-hub/issues   #
+#----------------------------------------------------------------------------------------#
+
+source /server-hub/qemu-kvm-manage/scripts-to-manage-vms/functions/defaults.sh
+
+# Function to show help
+fn_show_help() {
+    cat <<EOF
+Usage: kvm-reboot [hostname]
+
+Arguments:
+  hostname  Name of the VM to do graceful reboot (optional, will prompt if not given)
+EOF
+}
+
+# Handle help and argument validation
+if [[ $# -gt 1 ]]; then
+    echo -e "❌ Too many arguments.\n"
+    fn_show_help
+    exit 1
+fi
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    fn_show_help
+    exit 0
+fi
+
+if [[ "$1" == -* ]]; then
+    echo -e "❌ No such option: $1\n"
+    fn_show_help
+    exit 1
+fi
+
+# Use first argument or prompt for hostname
+source /server-hub/qemu-kvm-manage/scripts-to-manage-vms/functions/input-hostname.sh "$1"
+
+# Check if VM exists in 'virsh list --all'
+if ! sudo virsh list --all | awk '{print $2}' | grep -Fxq "$qemu_kvm_hostname"; then
+    echo "❌ Error: VM \"$qemu_kvm_hostname\" does not exist."
+    exit 1
+fi
+
+# Check if VM exists in 'virsh list'
+if ! sudo virsh list  | awk '{print $2}' | grep -Fxq "$qemu_kvm_hostname"; then
+    echo "❌ Error: VM \"$qemu_kvm_hostname\" is not running."
+    exit 1
+fi
+
+# Proceed with reboot
+sudo virsh reboot "${qemu_kvm_hostname}" 2>/dev/null
+
+echo -e "✅ VM \"$qemu_kvm_hostname\" is rebooted successfully. \n"
