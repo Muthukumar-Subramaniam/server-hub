6c168936191df22fd5f97f426942fd7b133bfeb8
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-12-02 08:59:56 +0000

    Update color functions: swap info and notify colors
    - print_info() now uses cyan (was blue)
    - print_notify() now uses white (was cyan)
    - Added MAKE_IT_WHITE color code

--- common-utils/color-functions.sh
+++ common-utils/color-functions.sh
@@ -4,6 +4,7 @@ MAKE_IT_GREEN='\033[0;32m'
 MAKE_IT_YELLOW='\033[0;33m'
 MAKE_IT_BLUE='\033[0;34m'
 MAKE_IT_CYAN='\033[0;36m'
+MAKE_IT_WHITE='\033[0;37m'
 MAKE_IT_MAGENTA='\033[0;35m'
 RESET_COLOR='\033[0m' # Reset to default color
 
@@ -37,17 +38,17 @@ print_warning() {
 print_info() {
 	if [[ -z "${2}" ]] || [[ "${2}" != "nskip" ]] 
 	then
-		echo -e "${MAKE_IT_BLUE}${1}${RESET_COLOR}"
+		echo -e "${MAKE_IT_CYAN}${1}${RESET_COLOR}"
 	else
-		echo -ne "${MAKE_IT_BLUE}${1}${RESET_COLOR}"
+		echo -ne "${MAKE_IT_CYAN}${1}${RESET_COLOR}"
 	fi
 }
 
 print_notify() {
 	if [[ -z "${2}" ]] || [[ "${2}" != "nskip" ]] 
 	then
-		echo -e "${MAKE_IT_CYAN}${1}${RESET_COLOR}"
+		echo -e "${MAKE_IT_WHITE}${1}${RESET_COLOR}"
 	else
-		echo -ne "${MAKE_IT_CYAN}${1}${RESET_COLOR}"
+		echo -ne "${MAKE_IT_WHITE}${1}${RESET_COLOR}"
 	fi
 }
