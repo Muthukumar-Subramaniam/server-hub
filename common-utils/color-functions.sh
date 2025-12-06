84ee28cf21d5642a4eb05d2d6184a6a6e2c562d3
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-12-06 04:03:08 +0000

    refactor(color-functions): change [INFO] color from white to blue
    - Changed print_info to use blue color instead of white
    - Avoids conflict with [SUMMARY] which uses white
    - Blue is distinct and appropriate for informational messages

--- common-utils/color-functions.sh
+++ common-utils/color-functions.sh
@@ -47,9 +47,9 @@ print_notify() {
 print_info() {
 	if [[ -z "${2:-}" ]] || [[ "${2:-}" != "nskip" ]] 
 	then
-		echo -e "${MAKE_IT_WHITE}[INFO] ${1}${RESET_COLOR}"
+		echo -e "${MAKE_IT_BLUE}[INFO] ${1}${RESET_COLOR}"
 	else
-		echo -ne "${MAKE_IT_WHITE}[INFO] ${1}${RESET_COLOR}"
+		echo -ne "${MAKE_IT_BLUE}[INFO] ${1}${RESET_COLOR}"
 	fi
 }
 
