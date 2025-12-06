e925949866cc69a8d7c2a206cf1fae5c91fed63e
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2025-12-06 03:49:41 +0000

    fix(kvm-stop): use print_warning for SKIP summary line
    - Changed from print_skip to print_warning for consistency with kvm-start
    - Removes double [SKIP] [SKIP] issue
    - Summary SKIP line now shows [WARN] SKIP: consistent with other tools

--- qemu-kvm-manage/scripts-to-manage-vms/kvm-stop.sh
+++ qemu-kvm-manage/scripts-to-manage-vms/kvm-stop.sh
@@ -121,7 +121,7 @@ if [[ -n "$hosts_list" ]]; then
         print_success "  DONE: ${#successful_vms[@]}/$total_vms (${successful_vms[*]})"
     fi
     if [[ ${#skipped_vms[@]} -gt 0 ]]; then
-        print_skip "  SKIP: ${#skipped_vms[@]}/$total_vms (${skipped_vms[*]})"
+        print_warning "  SKIP: ${#skipped_vms[@]}/$total_vms (${skipped_vms[*]})"
     fi
     if [[ ${#failed_vms[@]} -gt 0 ]]; then
         print_error "  FAIL: ${#failed_vms[@]}/$total_vms (${failed_vms[*]})"
